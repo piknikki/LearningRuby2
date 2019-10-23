8cf5f463c69600ea6eac2898bc5c9ef3de573316
Nikki <piknik@me.com>
2019-10-23 13:43:34 -0600

    more eaches for sneeches

---
+++ each_addition.rb
@@ -0,0 +1,14 @@
+# array = %w(pug purple biscuits pigeon)
+#
+# array.each do |team|
+#   puts "The current team is #{team}"
+# end
+#
+array = [0, 1, 2, 3, 4, 5]
+
+array.each do |item|
+  item = item + 2
+  puts "The current item + 2 is #{item}"
+end
+
+puts array.inspect
